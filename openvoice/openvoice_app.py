22bda6215e2cd764e438c9edf4f4a7354a15a6e3
me-osamu-dazai <136231027+me-osamu-dazai@users.noreply.github.com>
2024-05-10 15:05:49 +0530

    Removed 200 characters restriction

--- openvoice/openvoice_app.py
+++ openvoice/openvoice_app.py
@@ -102,16 +102,6 @@ def predict(prompt, style, audio_file_pth, agree):
             None,
             None,
         )
-    if len(prompt) > 200:
-        text_hint += f"[ERROR] Text length limited to 200 characters for this demo, please try shorter text. You can clone our open-source repo and try for your usage \n"
-        gr.Warning(
-            "Text length limited to 200 characters for this demo, please try shorter text. You can clone our open-source repo for your usage"
-        )
-        return (
-            text_hint,
-            None,
-            None,
-        )
     
     # note diffusion_conditioning not used on hifigan (default mode), it will be empty but need to pass it to model.inference
     try:
